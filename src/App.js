af21868d19d0f51c201b08a35b7e983c9b539706
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-05-25 15:44:48 +0100

    Background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import css from "./App.module.css";
+import logo from "./assets/snowflake.png";
 import HyggeList from "./components/HyggeList";
 
 const App = () => {
   return (
     <main className={css.App}>
       <h1 className={css.Heading}>~ your hygge ~</h1>
+      <img className={css.Logo} src={logo} alt="Logo" />
       <HyggeList list={[{ id: 1 }, { id: 2 }, { id: 3 }]} />
     </main>
   );
