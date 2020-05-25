8c54224eafe3204ef4131feffbe707c882acd125
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-05-25 17:42:27 +0100

    Updates

---
+++ precache-manifest.926295a97329adb248e9bfbbbfd93b57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede78ab3c1f09f711108314a81bc1228",
+    "url": "/hygge/index.html"
+  },
+  {
+    "revision": "d63c03f96abd250fafda",
+    "url": "/hygge/static/css/main.14346b24.chunk.css"
+  },
+  {
+    "revision": "58c66a82e41a245ab405",
+    "url": "/hygge/static/js/2.888a5146.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hygge/static/js/2.888a5146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63c03f96abd250fafda",
+    "url": "/hygge/static/js/main.20e6181f.chunk.js"
+  },
+  {
+    "revision": "2edaaa29c3450003587e",
+    "url": "/hygge/static/js/runtime-main.c02cf71d.js"
+  },
+  {
+    "revision": "f29ec4cf2f57ada98a735329ec377e89",
+    "url": "/hygge/static/media/snowflake.f29ec4cf.png"
+  }
+]);
