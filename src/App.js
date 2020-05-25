c43514ac29930db9a80507ea67244fb13326ee28
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-05-25 17:02:23 +0100

    Improved look on Hygge expansion

--- src/App.js
+++ src/App.js
@@ -10,23 +10,35 @@ const App = () => {
     { id: 2, display: true, isExpanded: false },
     { id: 3, display: true, isExpanded: false },
   ]);
+  const [hasExpanded, setHasExpanded] = useState(false);
 
   const clickHyggeHandler = (id) => {
+    let updatedExp = false;
+
     const updatedHygges = hygges.map((hg) => {
       const newHg = {
         ...hg,
         display: hg.id !== id ? !hg.display : hg.display,
         isExpanded: hg.id === id ? !hg.isExpanded : hg.isExpanded,
       };
+
+      updatedExp = newHg.isExpanded ? newHg.isExpanded : updatedExp;
+
       return newHg;
     });
 
     setHygges(updatedHygges);
+    setHasExpanded(updatedExp);
   };
+
+  const headerClasses = [css.Heading];
+  if (hasExpanded) {
+    headerClasses.push(css.Expanded);
+  }
 
   return (
     <main className={css.App}>
-      <h1 className={css.Heading}>~ your hygge ~</h1>
+      <h1 className={headerClasses.join(" ")}>~ your hygge ~</h1>
       <img className={css.Logo} src={logo} alt="Logo" />
       <HyggeList list={hygges} clickHygge={clickHyggeHandler} />
     </main>
