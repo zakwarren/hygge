3e4b0b945810ef59b29c06cab78a71c7e0228ed1
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-05-25 17:26:39 +0100

    Make installable

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import css from "./App.module.css";
+import downloadIcon from "./assets/download.png";
 import Hygge from "./containers/Hygge";
 
 const App = () => {
-  return <Hygge />;
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", getInstallPrompt);
+    return () => {
+      window.removeEventListener("beforeinstallprompt", getInstallPrompt);
+    };
+  }, []);
+
+  const getInstallPrompt = (event) => {
+    event.preventDefault();
+    setDeferredPrompt(event);
+    return false;
+  };
+
+  const promptForInstallHandler = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      setDeferredPrompt(null);
+    }
+  };
+
+  let downloadBtn = null;
+  if (deferredPrompt) {
+    downloadBtn = (
+      <button className={css.DownloadBtn} onClick={promptForInstallHandler}>
+        <img src={downloadIcon} alt="Download" />
+      </button>
+    );
+  }
+
+  return (
+    <>
+      <Hygge />
+      {downloadBtn}
+    </>
+  );
 };
 
 export default App;
