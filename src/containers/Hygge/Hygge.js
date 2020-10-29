8cf6b77c4548ac4802857d4e303e606cac0f7861
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-10-29 20:43:39 +0000

    Update hygge

--- src/containers/Hygge/Hygge.js
+++ src/containers/Hygge/Hygge.js
@@ -1,45 +1,50 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 import css from "./Hygge.module.css";
 import logo from "../../assets/snowflake.png";
-import { IMAGE_MAPPING } from "../../shared/images";
+import * as actions from "../../store/actions/index";
+import { expandHygge } from "../../shared/utilities";
 import HyggeList from "../../components/Hygge/HyggeList";
 import Heading from "../../components/Heading/Heading";
 
-const IMAGES = IMAGE_MAPPING.map((map) => {
-  return { ...map, display: true, isExpanded: false };
-});
-
-const Hygge = () => {
-  const [hygges, setHygges] = useState(IMAGES);
+export const Hygge = (props) => {
+  const { collection, onSetCollection } = props;
   const [hasExpanded, setHasExpanded] = useState(false);
 
   const clickHyggeHandler = (id) => {
-    let updatedExp = false;
-
-    const updatedHygges = hygges.map((hg) => {
-      const newHg = {
-        ...hg,
-        display: hg.id !== id ? !hg.display : hg.display,
-        isExpanded: hg.id === id ? !hg.isExpanded : hg.isExpanded,
-      };
-
-      updatedExp = newHg.isExpanded ? newHg.isExpanded : updatedExp;
-
-      return newHg;
-    });
-
-    setHygges(updatedHygges);
-    setHasExpanded(updatedExp);
+    const newCollection = expandHygge(collection, id);
+    onSetCollection(newCollection);
+    const areExpanded = newCollection.some((hg) => hg.isExpanded);
+    setHasExpanded(areExpanded);
   };
 
   return (
     <main className={css.Hygge}>
       <Heading headerText="Hygge Board" hasExpanded={hasExpanded} />
       <img className={css.Logo} src={logo} alt="Logo" />
-      <HyggeList list={hygges} clickHygge={clickHyggeHandler} />
+      <HyggeList list={collection} clickHygge={clickHyggeHandler} />
     </main>
   );
 };
 
-export default Hygge;
+Hygge.propTypes = {
+  collection: PropTypes.array.isRequired,
+  onSetCollection: PropTypes.func.isRequired,
+};
+
+export const mapStateToProps = (state) => {
+  return {
+    collection: state.hygge.collection,
+  };
+};
+
+export const mapDispatchToProps = (dispatch) => {
+  return {
+    onSetCollection: (newCollection) =>
+      dispatch(actions.setCollection(newCollection)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Hygge);
--- src/containers/Hygge/Hygge.test.js
+++ src/containers/Hygge/Hygge.test.js
@@ -2,41 +2,67 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import css from "./Hygge.module.css";
-import logo from "../assets/snowflake.png";
-import Hygge from "./Hygge";
-import HyggeList from "../components/HyggeList";
+import logo from "../../assets/snowflake.png";
+import { Hygge, mapStateToProps, mapDispatchToProps } from "./Hygge";
+import HyggeList from "../../components/Hygge/HyggeList";
 import Heading from "../../components/Heading/Heading";
 
 describe("<Hygge />", () => {
-  let wrapper;
+  describe("mapStateToProps", () => {
+    it("should map the state to props correctly", () => {
+      const hygge = {
+        collection: [],
+      };
+      const appState = { hygge: hygge };
+      const componentState = mapStateToProps(appState);
 
-  beforeEach(() => {
-    wrapper = shallow(<Hygge />);
+      expect(componentState).toEqual(hygge);
+    });
   });
 
-  it("should render a <main /> element with the Hygge class", () => {
-    const main = wrapper.find("main");
+  describe("mapDispatchToProps", () => {
+    it("should map the dispatch functions to props correctly", () => {
+      const componentDispatch = mapDispatchToProps(jest.fn);
 
-    expect(main).toHaveLength(1);
-    expect(main.hasClass(css.Hygge)).toEqual(true);
+      expect(typeof componentDispatch.onSetCollection).toBe("function");
+    });
   });
 
-  it("should render a <Heading /> element", () => {
-    const header = wrapper.find(Heading);
+  describe("display", () => {
+    let wrapper;
+    const collection = [];
+    const onSetCollection = jest.fn;
 
-    expect(header).toHaveLength(1);
-  });
+    beforeEach(() => {
+      wrapper = shallow(
+        <Hygge collection={collection} onSetCollection={onSetCollection} />
+      );
+    });
 
-  it("should render a <img /> element with the Logo class and correct src", () => {
-    const img = wrapper.find("img");
+    it("should render a <main /> element with the Hygge class", () => {
+      const main = wrapper.find("main");
 
-    expect(img).toHaveLength(1);
-    expect(img.hasClass(css.Logo)).toEqual(true);
-    expect(img.prop("src")).toEqual(logo);
-    expect(img.prop("alt")).toEqual("Logo");
-  });
+      expect(main).toHaveLength(1);
+      expect(main.hasClass(css.Hygge)).toEqual(true);
+    });
 
-  it("should render a <HyggeList /> element", () => {
-    expect(wrapper.find(HyggeList)).toHaveLength(1);
+    it("should render a <Heading /> element", () => {
+      const header = wrapper.find(Heading);
+
+      expect(header).toHaveLength(1);
+    });
+
+    it("should render a <img /> element with the Logo class and correct src", () => {
+      const img = wrapper.find("img");
+
+      expect(img).toHaveLength(1);
+      expect(img.hasClass(css.Logo)).toEqual(true);
+      expect(img.prop("src")).toEqual(logo);
+      expect(img.prop("alt")).toEqual("Logo");
+    });
+
+    it("should render a <HyggeList /> element", () => {
+      expect(wrapper.find(HyggeList)).toHaveLength(1);
+    });
   });
 });
