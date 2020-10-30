e07793a282cfe9d201eaa9052bba57a3c3e9bb35
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-10-30 18:37:18 +0000

    Updates

--- precache-manifest.js
+++ precache-manifest.js
@@ -5,64 +5,64 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     revision: "bb324b3062227a5754c6",
-    url: "/hygge/static/css/main.36b980b8.chunk.css",
+    url: "/hygge/static/css/main.6cdcc2ba.chunk.css",
   },
   {
     revision: "40c89e47914c5503c098",
-    url: "/hygge/static/js/2.d547edfc.chunk.js",
+    url: "/hygge/static/js/2.6db76eaa.chunk",
   },
   {
     revision: "c64c486544348f10a6d6c716950bc223",
-    url: "/hygge/static/js/2.d547edfc.chunk.js.LICENSE.txt",
+    url: "/hygge/static/js/2.6db76eaa.chunk.LICENSE.txt",
   },
   {
     revision: "bb324b3062227a5754c6",
-    url: "/hygge/static/js/main.6223fb61.chunk.js",
+    url: "/hygge/static/js/main.991d0815.chunk.js",
   },
   {
     revision: "2edaaa29c3450003587e",
-    url: "/hygge/static/js/runtime-main.c02cf71d.js",
+    url: "/hygge/static/js/runtime-main.00cbb7cf.js",
   },
   {
     revision: "d718e664ef038bcc18c903f77a81ddeb",
-    url: "/hygge/static/media/aditya-vyas-PzhmEp_aDU4-unsplash.d718e664.jpg",
+    url: "/hygge/static/media/aditya-vyas-PzhmEp_aDU4-unsplash.45437293.jpg",
   },
   {
     revision: "844353bb66c109cc38e151ea312ae203",
-    url: "/hygge/static/media/annie-spratt-OQj0chxpZOI-unsplash.844353bb.jpg",
+    url: "/hygge/static/media/annie-spratt-OQj0chxpZOI-unsplash.21ff2d34.jpg",
   },
   {
     revision: "6d0d1780d450a7ed42e3c23abc5a95d0",
-    url: "/hygge/static/media/clay-banks-z_DkoUqgx6M-unsplash.6d0d1780.jpg",
+    url: "/hygge/static/media/clay-banks-z_DkoUqgx6M-unsplash.6a6ef3b8.jpg",
   },
   {
     revision: "eaa91b6df0e746e3315cb8c42d04f891",
-    url: "/hygge/static/media/emily-toycen-jd_YFWRkOqQ-unsplash.eaa91b6d.jpg",
+    url: "/hygge/static/media/emily-toycen-jd_YFWRkOqQ-unsplash.2d490550.jpg",
   },
   {
     revision: "7090588136bba7e46526faed63969ee9",
-    url: "/hygge/static/media/john-noonan-E-9CFTftQh4-unsplash.70905881.jpg",
+    url: "/hygge/static/media/john-noonan-E-9CFTftQh4-unsplash.8315c40c.jpg",
   },
   {
     revision: "17eb292ce533935f2c58b9396ad32b7b",
-    url: "/hygge/static/media/jyrki-sorjonen-WnWwzDxvA2M-unsplash.17eb292c.jpg",
+    url: "/hygge/static/media/jyrki-sorjonen-WnWwzDxvA2M-unsplash.f8f70003.jpg",
   },
   {
     revision: "0851b697f6236e0d3d5034b3ef957e4c",
     url:
-      "/hygge/static/media/olivier-guillard-FKJgBUDoVC0-unsplash.0851b697.jpg",
+      "/hygge/static/media/olivier-guillard-FKJgBUDoVC0-unsplash.af5bba6e.jpg",
   },
   {
     revision: "863a8c70f4195ca904d3e7f76855cea1",
     url:
-      "/hygge/static/media/sincerely-media-9nhxEa3PK30-unsplash.863a8c70.jpg",
+      "/hygge/static/media/sincerely-media-9nhxEa3PK30-unsplash.f373a935.jpg",
   },
   {
     revision: "f29ec4cf2f57ada98a735329ec377e89",
-    url: "/hygge/static/media/snowflake.f29ec4cf.png",
+    url: "/hygge/static/media/snowflake.9099a541.png",
   },
   {
     revision: "b6bc7963b5af78e944d4a1617d4f31b2",
-    url: "/hygge/static/media/stephane-juban-DI8Bf6K1134-unsplash.b6bc7963.jpg",
+    url: "/hygge/static/media/stephane-juban-DI8Bf6K1134-unsplash.a27832e1.jpg",
   },
 ]);
