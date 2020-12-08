68b259b63f1682f00d545bd5c22facd1aec18810
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-12-08 21:12:56 +0000

    App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,38 @@
 import React from "react";
 import { shallow } from "enzyme";
+import * as redux from "react-redux";
 
 import App from "./App";
+import css from "./App.module.css";
 import Menu from "./containers/Menu/Menu";
+import logo from "./assets/snowflake.png";
 
 describe("<App />", () => {
   let wrapper;
 
   beforeEach(() => {
+    const spy = jest.spyOn(redux, "useDispatch");
+    spy.mockReturnValue(jest.fn);
+
     wrapper = shallow(<App />);
   });
 
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it("should render a <Menu /> element", () => {
-    expect(wrapper.find(Menu)).toHaveLength(1);
+    const menu = wrapper.find(Menu);
+
+    expect(menu).toHaveLength(1);
+  });
+
+  it("should render an <img /> element with the correct class and src", () => {
+    const img = wrapper.find("img");
+
+    expect(img).toHaveLength(1);
+    expect(img.hasClass(css.Logo)).toEqual(true);
+    expect(img.prop("src")).toEqual(logo);
+    expect(img.prop("alt")).toEqual("Logo");
   });
 });
